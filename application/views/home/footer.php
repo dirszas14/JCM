c6b6c30052bdb6ba4f280d26d976b17664beddb8
dirszas14 <31124351+dirszas14@users.noreply.github.com>
2018-02-09 08:57:50 +0700

    first commit

---
+++ application/views/home/footer.php
@@ -0,0 +1,7 @@
+</body>
+<script src="<?php echo base_url('assets/plugins/jquery/dist/jquery.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/plugins/bootstrap/dist/js/bootstrap.min.js')?>" ></script>
+<script src="<?php echo base_url('assets/plugins/datatables.net/js/jquery.dataTables.min.js')?>"></script>
+<script src="<?php echo base_url('assets/plugins/datatables.net-bs/js/dataTables.bootstrap.min.js')?>"></script>
+<script src="<?php echo base_url('assets/dist/b4/js/bootstrap.min.js') ?>"></script>
+</html>
