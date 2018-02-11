8ffa9956cc1f65b6c46394d1783d576ee6ea368e
dirszas14 <31124351+dirszas14@users.noreply.github.com>
2018-02-11 18:06:55 +0700

    ionicon & jquery

--- application/views/home/content.php
+++ application/views/home/content.php
@@ -1,3 +1,9 @@
+<div class="container-fluid">
+	<div class="container">
+		<nav class="navbar navbar-dark bg-dark"></nav>
+	</div>
+</div>
+
 <div class="container">
 	<div class="jumbotron">
   <h1 class="display-4">Hello, world!</h1>
@@ -18,7 +24,7 @@
 		</div>
 		<div class="col">
 			<div class="card">
-  <img class="card-img-top" src="..." alt="Card image cap">
+  <img class="card-img-top" src="https://images.pexels.com/photos/572938/pexels-photo-572938.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" alt="Card image cap">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
--- application/views/home/footer.php
+++ application/views/home/footer.php
@@ -1,7 +1,5 @@
 </body>
 <script src="<?php echo base_url('assets/plugins/jquery/dist/jquery.min.js') ?>"></script>
-<script src="<?php echo base_url('assets/plugins/bootstrap/dist/js/bootstrap.min.js')?>" ></script>
-<script src="<?php echo base_url('assets/plugins/datatables.net/js/jquery.dataTables.min.js')?>"></script>
-<script src="<?php echo base_url('assets/plugins/datatables.net-bs/js/dataTables.bootstrap.min.js')?>"></script>
-<script src="<?php echo base_url('assets/dist/b4/js/bootstrap.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/dist/b4/js/bootstrap.min.js')?>" ></script>
+
 </html>
