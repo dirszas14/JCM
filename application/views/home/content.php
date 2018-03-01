366bf26187910bbc9ee65d1c6bee0c18ffb61399
alief9393 <aliefchandra10@gmail.com>
2018-03-01 11:40:36 +0700

    carousel & footer

--- application/views/home/content.php
+++ application/views/home/content.php
@@ -1,102 +1,123 @@
 <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
-	<a class="navbar-brand" href="#">Jakarta Center Management</a>
+  <a class="navbar-brand" href="#">Jakarta Center Management</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+  <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav ml-auto">
       <li class="nav-item active">
-        <a class="navbar-list" href="#">Anggota</a>
+        <a class="navbar-list" href="#">Daftar</a>
+      </li>
+      <li class="nav-item active">
+        <a class="navbar-list" href="#anggota">Anggota</a>
       </li>
       <li class="nav-item">
         <a class="navbar-list" href="#">Kontak</a>
-      </li>	
+      </li>
     </ul>
   </div>
 </nav>
-
-<section class="canvas-head text-center">
-  <div class="container-fluid">
-  <div class="row">
-  <div class="col-md-8 offset-md-2 cover-head">
-  <h1 style="color:white">Jakarta Center Management</h1>
-  <h3 style="color: white;">Jakarta Tengah Manajemen</h3>
-  </div>
+<section class="carousel">
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+      <div class="carousel-item active">
+        <img class="d-block img-fluid" src="<?php echo base_url('assets/img/model4.jpg') ?>" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block img-fluid" src="<?php echo base_url('assets/img/model6.jpeg') ?>" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block img-fluid" src="<?php echo base_url('assets/img/model5.jpeg') ?>" alt="Third slide">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
 </section>
-</div>
-  <section class="canvas-visimisi text-center">
-    <blockquote class="blockquote">
-  <h3 class="mb-0">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."</h3>
+<br>
+<section class="canvas-visimisi text-center">
+  <blockquote class="blockquote">
+    <h3 class="mb-0">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."</h3>
   <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
 </blockquote>
-    <h1 class="display-3"><u>VISI</u></h1>
-    <div class="container">
-        <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum maiores optio laudantium earum vel, soluta itaque dolorem quidem ipsum sint rerum fugit asperiores, in porro unde sunt, illo quo ipsa.</p>
-        </div> 
-        <h1 class="display-3"><u>MISI</u></h1>
-    <div class="container">
-        <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum maiores optio laudantium earum vel, soluta itaque dolorem quidem ipsum sint rerum fugit asperiores, in porro unde sunt, illo quo ipsa.</p>
-        </div> 
-  </section>
-  <section class="canvas-anggota text-center">
-    <h1 class="">Anggota</h1>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-6">
-          <div class="canvas-classa">
+<h1 class="display-3"><u>VISI</u></h1>
+<div class="container">
+  <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum maiores optio laudantium earum vel, soluta itaque dolorem quidem ipsum sint rerum fugit asperiores, in porro unde sunt, illo quo ipsa.</p>
+</div>
+<h1 class="display-3"><u>MISI</u></h1>
+<div class="container">
+  <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum maiores optio laudantium earum vel, soluta itaque dolorem quidem ipsum sint rerum fugit asperiores, in porro unde sunt, illo quo ipsa.</p>
+</div>
+</section>
+<div id="anggota">
+<section class="canvas-anggota text-center">
+  <h1 class="">Anggota</h1>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="canvas-classa">
           <h2>CLASS A</h2>
           <div class="row">
             <div class="col-sm-6">
               <div class="card panel">
-  <img class="card-img-top" src="<?php echo base_url('assets/img/fototest.jpeg') ?>" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+                <img class="card-img-top" src="<?php echo base_url('assets/img/classA.jpg') ?>" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
             </div>
             <div class="col-sm-6">
               <div class="card panel">
-  <img class="card-img-top" src="<?php echo base_url('assets/img/fototest.jpeg') ?>" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+                <img class="card-img-top" src="<?php echo base_url('assets/img/classA.jpg') ?>" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
             </div>
           </div>
+          <a href="#" class="btn btn-primary">Lihat Semua</a>
         </div>
-        </div>
-        <div class="col-md-6">
-          <div class="canvas-classb">
+      </div>
+      <div class="col-md-6">
+        <div class="canvas-classb">
           <h2>CLASS B</h2>
-           <div class="row">
+          <div class="row">
             <div class="col">
               <div class="card panel">
-  <img class="card-img-top" src="<?php echo base_url('assets/img/fototest.jpeg') ?>" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+                <img class="card-img-top" src="<?php echo base_url('assets/img/classB.jpg') ?>" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
             </div>
             <div class="col">
               <div class="card panel">
-  <img class="card-img-top" src="<?php echo base_url('assets/img/fototest.jpeg') ?>" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-</div>
+                <img class="card-img-top" src="<?php echo base_url('assets/img/classB.jpg') ?>" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a href="#" class="btn btn-primary">Lihat Semua</a>
         </div>
       </div>
     </div>
   </div>
+</section>
 </div>
-  </section>
--- application/views/home/footer.php
+++ application/views/home/footer.php
@@ -1,5 +1,67 @@
+<br>
+<footer class="footer">
+	<div class="container-fluid bg-dark py-3">
+		<div class="container">
+			<div class="row py-3">
+				<div class="col-md-4 col-centered text-center">
+					<p class="text-white"></p>
+				</div>
+				<div class="col-md-4 col-centered text-center">
+					<ul class="social-network social-circle">
+						<li>
+							<a href="https://www.facebook.com/" class="icoFacebook"><i class="fa fa-facebook"></i>
+							</a>
+						</li>
+						<li>
+							<a href="https://www.instagram.com/" class="icoInstagram"><i class="fa fa-instagram"></i>
+							</a>
+						</li>
+						<li>
+							<a href="https://twitter.com/" class="icoTwitter">
+							<i class="fa fa-twitter"></i></a>
+						</li>
+						<li>
+							<a href="https://www.linkedin.com/company/" class="icoLinkedin">
+							<i class="fa fa-linkedin"></i></a>
+						</li>
+				</div>
+				<div class="col-md-4 col-centered text-center">
+					<p class="text-white"></p>
+				</div>
+			</div>
+			<div class="row py-3">
+				<div class="col-md-4 col-centered text-center">
+					<p class="text-white"></p>
+				</div>
+				<div class="col-md-4 col-centered text-center">
+					<p class="text-white">© 2018 Copyright : Jakarta Center Management</p>
+				</div>
+				<div class="col-md-4 col-centered text-center">
+					<p class="text-white"></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+<!--/Footer-->
 </body>
 <script src="<?php echo base_url('assets/plugins/jquery/dist/jquery.min.js') ?>"></script>
 <script src="<?php echo base_url('assets/dist/b4/js/bootstrap.min.js')?>" ></script>
-
 </html>
+<!-- <ul class="social-network social-circle">
+						<li>
+							<a href="https://www.facebook.com/" class="icoFacebook"><i class="fa fa-facebook"></i>
+							</a>
+						</li>
+						<li>
+							<a href="https://www.instagram.com/" class="icoInstagram"><i class="fa fa-instagram"></i>
+							</a>
+						</li>
+						<li>
+							<a href="https://twitter.com/" class="icoTwitter">
+							<i class="fa fa-twitter"></i></a>
+						</li>
+						<li>
+							<a href="https://www.linkedin.com/company/" class="icoLinkedin">
+							<i class="fa fa-linkedin"></i></a>
+						</li> -->
