cdcd082268c1de94c3be11306fbd9a3451fb025c
dirszas14 <31124351+dirszas14@users.noreply.github.com>
2018-03-08 22:24:24 +0700

    modify home 1

--- application/views/home/content.php
+++ application/views/home/content.php
@@ -1,44 +1,49 @@
-<nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
+<nav style="transition: padding .3s;" class="navbar navbar-expand-md fixed-top navbar-dark bg-transparent">
   <a class="navbar-brand" href="#">Jakarta Center Management</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="navbar-list" href="">Tentang</a>
-      </li>
-      <li class="nav-item active">
-        <a class="navbar-list" href="#anggota">Services</a>
+    <ul class="nav navbar-nav ml-auto nav-pills">
+      <li class="nav-item">
+        <a class="nav-link navbar-list" href="#tentang">Tentang</a>
       </li>
       <li class="nav-item">
-        <a class="navbar-list" href="#">Blog</a>
+        <a class="nav-link navbar-list" href="#services">Services</a>
       </li>
       <li class="nav-item">
-        <a class="navbar-list" href="#">Our Clients</a>
-      </li><li class="nav-item">
-        <a class="navbar-list" href="#">Contact</a>
+        <a class="nav-link navbar-list" href="#member">Member</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link navbar-list" href="#">Blog</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link navbar-list" href="#">Our Clients</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link navbar-list" href="#">Contact</a>
       </li>
     </ul>
   </div>
 </nav>
 
-<div class="jumbotron jumbotron-fluid  canvas-head">
-  
-  <div class="container cover-head">
+<div class="canvas-head" id="home">
+  <div class="cover-head">
+  <div class="container">
     <div class="cover-text">
   <h1 style="color: white;">Jakarta Center Management</h1>
   <p style="color: white;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti cupiditate, sapiente perferendis. Amet reprehenderit, provident reiciendis labore consequatur ut deserunt, id vero obcaecati maxime odio debitis quisquam facilis hic voluptatem.</p>
   <div class="row">
-    <div class="col-md-2"><a class="btn btn-primary btn-lg col-md-12" href="#" role="button">Link <i class="fas fa-arrow-right"></i></a></div>
-    <div class="col-md-2"><a class="btn btn-primary btn-lg col-md-12" href="#" role="button">Link <i class="fas fa-arrow-right"></i></a></div>
+    <div class="col-md-2"><a class="btn btn-info btn-lg col-md-12" href="#" role="button">Link <i class="fa fa-arrow-right"></i></a></div>
+    <div class="col-md-2"><a class="btn btn-warning btn-lg col-md-12" href="#" role="button">Link <i class="fa fa-arrow-right"></i></a></div>
+    </div>
   </div>
   </div>
   </div>
 </div>
 
 <br>
-<section class="canvas-visimisi text-center">
+<section class="canvas-visimisi text-center" id="tentang">
   <blockquote class="blockquote">
     <h3 class="mb-0">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."</h3>
   <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
@@ -52,7 +57,35 @@
         <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum maiores optio laudantium earum vel, soluta itaque dolorem quidem ipsum sint rerum fugit asperiores, in porro unde sunt, illo quo ipsa.</p>
         </div> 
   </section>
-  <section class="canvas-anggota text-center">
+  <section class="canvas-services text-center" id="services">
+    <div class="container">
+      <h1 class="display-2">Our Services</h1>
+      <p>Dengan kematangan pengalaman yang kami miliki disertai dengan semangat profesionalisme yang tinggi, kami berusaha memberikan yang terbaik pada setiap event dan pelayanan yang kami berikan. Kami selalu berkomitmen untuk selalu menjaga kepuasaan anda terhadap setiap jasa yang kami deliver demi menciptakan long term relationship. Senyum anda, kebanggan kami.</p>
+     <div class="row">
+   <div class="col">
+      <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+      <hr>
+      <h3>SPG &amp;SPB</h3>
+   </div>
+   <div class="col">
+      <i class="fa fa-user fa-3x" aria-hidden="true"></i>
+      <hr>
+      <h3>Usher</h3>
+   </div>
+   <div class="col">
+     <i class="fa fa-camera fa-3x" aria-hidden="true"></i>
+      <hr>
+      <h3>Modelling</h3>
+   </div>
+   <div class="col">
+      <i class="fa fa-music fa-3x" aria-hidden="true"></i>
+      <hr>
+      <h3>Band &amp; Singer</h3>
+   </div>
+</div>
+    </div>
+  </section>
+  <section class="canvas-anggota text-center" id="member">
     <h1 class="">Anggota</h1>
     <div class="container-fluid">
       <div class="row">
@@ -62,7 +95,7 @@
           <div class="row">
             <div class="col">
               <div class="card panel">
-                <img class="card-img-top" src="<?php echo base_url('assets/img/model7.jpeg') ?>" alt="Card image cap">
+                <img class="card-img-top" src="<?php echo base_url('assets/img/model1.jpeg') ?>" alt="Card image cap">
                 <div class="card-body">
                   <h5 class="card-title">Card title</h5>
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -111,4 +144,46 @@
     </div>
   </div>
 </section>
-</div>
+<section class="canvas-blog">
+  <h1 class="display-2 text-center">BLOG</h1>
+  <div class="container">
+    <div class="row ">
+      <div class="col">
+        <img class="img-fluid" src="<?php echo base_url('assets/img/photo3.jpg'); ?>" alt="">
+        <div class="row">
+          <div class="col">
+            <p class="font-weight-light"><i class="fa fa-user" aria-hidden="true"></i> admin</p>
+          </div>
+          <div class="col">
+            <p class="font-weight-light"><i class="fa fa-clock-o" aria-hidden="true"></i> 17 December 2017</p>
+          </div>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, neque. Aperiam possimus, ab vel explicabo distinctio ipsam perferendis non similique vitae id laborum mollitia repudiandae dolorum dolorem, pariatur, commodi libero.</p>
+        </div>
+      </div>
+      <div class="col">
+        <img class="img-fluid" src="<?php echo base_url('assets/img/photo3.jpg'); ?>" alt="">
+        <div class="row">
+          <div class="col">
+            <p class="font-weight-light"><i class="fa fa-user" aria-hidden="true">&nbsp;</i> admin</p>
+          </div>
+          <div class="col">
+            <p class="font-weight-light"><i class="fa fa-clock-o" aria-hidden="true"></i> 17 December 2017</p>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, neque. Aperiam possimus, ab vel explicabo distinctio ipsam perferendis non similique vitae id laborum mollitia repudiandae dolorum dolorem, pariatur, commodi libero.</p>
+        </div>
+      </div>
+      <div class="col">
+        <img class="img-fluid" src="<?php echo base_url('assets/img/photo3.jpg'); ?>" alt="">
+        <div class="row">
+          <div class="col">
+            <p class="font-weight-light"><i class="fa fa-user" aria-hidden="true"></i> admin</p>
+          </div>
+          <div class="col">
+            <p class="font-weight-light"><i class="fa fa-clock-o" aria-hidden="true"></i> 17 December 2017</p>
+          </div>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, neque. Aperiam possimus, ab vel explicabo distinctio ipsam perferendis non similique vitae id laborum mollitia repudiandae dolorum dolorem, pariatur, commodi libero.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
--- application/views/home/footer.php
+++ application/views/home/footer.php
@@ -47,4 +47,52 @@
 </body>
 <script src="<?php echo base_url('assets/plugins/jquery/dist/jquery.min.js') ?>"></script>
 <script src="<?php echo base_url('assets/dist/b4/js/bootstrap.min.js')?>" ></script>
+<script>
+	
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 650){
+      $('nav').addClass('shrink');
+    }
+    else{
+      $('nav').removeClass('shrink');
+    }
+  });
+
+  // Add scrollspy to <body>
+$('body').scrollspy({target: ".navbar", offset: 50});
+
+// Add smooth scrolling on all links inside the navbar
+$("#navbarNav a").on('click', function(event) {
+
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+    });
+
+  } // End if
+
+});
+var offset = 2000;
+
+$('.navbar li a').click(function(event) {
+    event.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView();
+    scrollBy(0, -offset);
+});
+
+</script>
 </html>
