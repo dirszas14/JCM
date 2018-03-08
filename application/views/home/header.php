54578dfa37508942f8320034cf561d3bbf7dddac
dirszas14 <31124351+dirszas14@users.noreply.github.com>
2018-03-08 12:17:48 +0700

    add script defer fa

--- application/views/home/header.php
+++ application/views/home/header.php
@@ -5,7 +5,7 @@
 	<title>Jakarta Center Management</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="<?php echo base_url('assets/dist/b4/css/bootstrap.min.css') ?>">
- <script defer src="<?php echo base_url('assets/plugins/font-awesome/svg-with-js/js/fontawesome-all.js') ?>"></script>
+<!--  <script defer src="<?php echo base_url('assets/plugins/font-awesome/svg-with-js/js/fontawesome-all.js') ?>"></script> -->
    <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/style.css') ?>">
    <link rel="stylesheet" href="<?php echo base_url('assets/plugins/font-awesome/css/font-awesome.min.css') ?>">
 </head>
