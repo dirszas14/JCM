8db3d74301c349232a80f35da01d9ff4b0be6ae0
dirszas14 <31124351+dirszas14@users.noreply.github.com>
2018-03-16 20:18:23 +0700

    link fixed

--- application/views/blog/header.php
+++ application/views/blog/header.php
@@ -2,25 +2,25 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Jakarta Center Management</title>
+  <title>Jakarta Center Management - Grade Mawar</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="<?php echo base_url('assets/dist/b4/css/bootstrap.min.css') ?>">
   <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
-       <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="<?php echo base_url('assets/dist/b4/css/bootstrap.min.css') ?>">
 <!--  <script defer src="<?php echo base_url('assets/plugins/font-awesome/svg-with-js/js/fontawesome-all.js') ?>"></script> -->
    <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/style.css') ?>">
+     <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
    <link rel="stylesheet" href="<?php echo base_url('assets/plugins/font-awesome/css/font-awesome.min.css') ?>">
 </head>
-<body data-spy="scroll" data-target=".navbar" data-offset="199">
-    <nav style="transition: padding .3s;" class="navbar navbar-expand-md fixed-top navbar-dark shrink">
+<body data-spy="scroll" data-target=".navbar" data-offset="199" >
+  <nav style="transition: padding .3s;" class="navbar navbar-expand-md fixed-top navbar-dark shrink">
   <a class="navbar-brand" href="<?php echo site_url('') ?>">Jakarta Center Management</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="nav navbar-nav ml-auto nav-pills">
-       <li class="nav-item">
+      <li class="nav-item">
         <a class="nav-link navbar-list" href="<?php echo site_url('/#tentang') ?>">About Us</a>
       </li>
       <li class="nav-item">
@@ -40,4 +40,4 @@
       </li>
     </ul>
   </div>
-</nav> 
+</nav>
