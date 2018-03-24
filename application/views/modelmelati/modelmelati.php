070b7b7f2337d6891d98e06329ec4a41ffd62ce1
dirszas14 <31124351+dirszas14@users.noreply.github.com>
2018-03-24 20:16:52 +0700

    fixed not responsive

--- application/views/modelmelati/modelmelati.php
+++ application/views/modelmelati/modelmelati.php
@@ -1,7 +1,7 @@
 		</div>
-		<div class="col-10">
+		<div class="col-12 col-md-10">
 			<div class="row pb-3">
-				<div class="col">
+				<div class="col-12 col-md-4">
 					<img class="img-fluid model-photo" src="<?php echo base_url('assets/img/model1.jpeg') ?>" alt="Card image cap">
 					<p>Nama:Chelsea Elizabeth Islan</p>
 					<p>Tinggi: 175cm</p>
@@ -10,14 +10,14 @@
 
 
 				</div>
-				<div class="col">
+				<div class="col-12 col-md-4">
 					<img class="img-fluid model-photo" src="<?php echo base_url('assets/img/model5.jpeg') ?>" alt="Card image cap">
 					<p>Nama:Chelsea Elizabeth Islan</p>
 					<p>Tinggi: 175cm</p>
 					<p>Berat Badan: 65kg</p>
 					<p>Usia:22</p>
 				</div>
-				<div class="col">
+				<div class="col-12 col-md-4">
 					<img class="img-fluid model-photo" src="<?php echo base_url('assets/img/model7.jpeg') ?>" alt="Card image cap">
 					<p>Nama:Chelsea Elizabeth Islan</p>
 					<p>Tinggi: 175cm</p>
@@ -27,46 +27,21 @@
 
 			</div>
 			<div class="row pb-3">
-				<div class="col">
+				<div class="col-12 col-md-4">
 					<img class="img-fluid model-photo" src="<?php echo base_url('assets/img/model1.jpeg') ?>" alt="Card image cap">
 					<p>Nama:Chelsea Elizabeth Islan</p>
 					<p>Tinggi: 175cm</p>
 					<p>Berat Badan: 65kg</p>
 					<p>Usia:22</p>
 				</div>
-				<div class="col">
+				<div class="col-12 col-md-4">
 					<img class="img-fluid model-photo" src="<?php echo base_url('assets/img/model1.jpeg') ?>" alt="Card image cap">
 					<p>Nama:Chelsea Elizabeth Islan</p>
 					<p>Tinggi: 175cm</p>
 					<p>Berat Badan: 65kg</p>
 					<p>Usia:22</p>
 				</div>
-				<div class="col">
-					<img class="img-fluid model-photo" src="<?php echo base_url('assets/img/model1.jpeg') ?>" alt="Card image cap">
-					<p>Nama:Chelsea Elizabeth Islan</p>
-					<p>Tinggi: 175cm</p>
-					<p>Berat Badan: 65kg</p>
-					<p>Usia:22</p>
-				</div>
-				
-
-			</div>
-			<div class="row pb-3">
-				<div class="col">
-					<img class="img-fluid model-photo" src="<?php echo base_url('assets/img/model1.jpeg') ?>" alt="Card image cap">
-					<p>Nama:Chelsea Elizabeth Islan</p>
-					<p>Tinggi: 175cm</p>
-					<p>Berat Badan: 65kg</p>
-					<p>Usia:22</p>
-				</div>
-				<div class="col">
-					<img class="img-fluid model-photo" src="<?php echo base_url('assets/img/model1.jpeg') ?>" alt="Card image cap">
-					<p>Nama:Chelsea Elizabeth Islan</p>
-					<p>Tinggi: 175cm</p>
-					<p>Berat Badan: 65kg</p>
-					<p>Usia:22</p>
-				</div>
-				<div class="col">
+				<div class="col-12 col-md-4">
 					<img class="img-fluid model-photo" src="<?php echo base_url('assets/img/model1.jpeg') ?>" alt="Card image cap">
 					<p>Nama:Chelsea Elizabeth Islan</p>
 					<p>Tinggi: 175cm</p>
@@ -77,21 +52,46 @@
 
 			</div>
 			<div class="row pb-3">
-				<div class="col">
+				<div class="col-12 col-md-4">
 					<img class="img-fluid model-photo" src="<?php echo base_url('assets/img/model1.jpeg') ?>" alt="Card image cap">
 					<p>Nama:Chelsea Elizabeth Islan</p>
 					<p>Tinggi: 175cm</p>
 					<p>Berat Badan: 65kg</p>
 					<p>Usia:22</p>
 				</div>
-				<div class="col">
+				<div class="col-12 col-md-4">
 					<img class="img-fluid model-photo" src="<?php echo base_url('assets/img/model1.jpeg') ?>" alt="Card image cap">
 					<p>Nama:Chelsea Elizabeth Islan</p>
 					<p>Tinggi: 175cm</p>
 					<p>Berat Badan: 65kg</p>
 					<p>Usia:22</p>
 				</div>
-				<div class="col">
+				<div class="col-12 col-md-4">
+					<img class="img-fluid model-photo" src="<?php echo base_url('assets/img/model1.jpeg') ?>" alt="Card image cap">
+					<p>Nama:Chelsea Elizabeth Islan</p>
+					<p>Tinggi: 175cm</p>
+					<p>Berat Badan: 65kg</p>
+					<p>Usia:22</p>
+				</div>
+				
+
+			</div>
+			<div class="row pb-3">
+				<div class="col-12 col-md-4">
+					<img class="img-fluid model-photo" src="<?php echo base_url('assets/img/model1.jpeg') ?>" alt="Card image cap">
+					<p>Nama:Chelsea Elizabeth Islan</p>
+					<p>Tinggi: 175cm</p>
+					<p>Berat Badan: 65kg</p>
+					<p>Usia:22</p>
+				</div>
+				<div class="col-12 col-md-4">
+					<img class="img-fluid model-photo" src="<?php echo base_url('assets/img/model1.jpeg') ?>" alt="Card image cap">
+					<p>Nama:Chelsea Elizabeth Islan</p>
+					<p>Tinggi: 175cm</p>
+					<p>Berat Badan: 65kg</p>
+					<p>Usia:22</p>
+				</div>
+				<div class="col-12 col-md-4">
 					<img class="img-fluid model-photo" src="<?php echo base_url('assets/img/model1.jpeg') ?>" alt="Card image cap">
 					<p>Nama:Chelsea Elizabeth Islan</p>
 					<p>Tinggi: 175cm</p>
